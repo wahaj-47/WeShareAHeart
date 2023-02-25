af9e30045131d580107cd05601b1e9d3d03738d0
Wahaj Hussain <wahajhssn@gmail.com>
2023-02-24 23:59:33 -0600

    Player movement and health system (basic implementation)

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public int playerId = 1;
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Player" + playerId + "Horizontal");
+        movement.y = Input.GetAxisRaw("Player" + playerId + "Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
