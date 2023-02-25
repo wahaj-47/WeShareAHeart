17574a692e5107d75400c7f55b865077e4bbd41e
Wahaj Hussain <wahajhssn@gmail.com>
2023-02-25 10:53:58 -0600

    Start screen placeholder

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Animator animator;
+
+    public void LoadNextLevel(string transition = "Start")
     {
-        
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1, transition));
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Reload(string transition = "Start")
     {
-        
+        //AudioManager.instance.Play("Theme");
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex, transition));
     }
+
+    public void RestartGame(string transition = "Start")
+    {
+        StartCoroutine(LoadLevel(0, transition));
+    }
+
+    IEnumerator LoadLevel(int levelIndex, string transition)
+    {
+        animator.SetTrigger(transition);
+
+        yield return new WaitForSeconds(0.5f);
+
+        SceneManager.LoadScene(levelIndex);
+    }
+
 }
